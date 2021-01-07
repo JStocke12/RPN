2f361bfadf80df6ea4a07ea0069b27b28dfd422c
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-01-07 15:01:24 -0500

    Added square root function

--- RPN.py
+++ RPN.py
@@ -21,6 +21,12 @@ while True:
                 RPNOutput.append(val)
             elif i == "exit":
                 break
+            elif i == "sqrt":
+                approx = 1
+                val = RPNOutput.pop()
+                while abs(approx**2 - val) > 10**-15:
+                    approx = (approx + val/approx)/2
+                RPNOutput.append(approx)
             elif i in OpList:
                 RPNOutput.append(OpList[i](RPNOutput.pop(),RPNOutput.pop()))
             elif i in SubList:
